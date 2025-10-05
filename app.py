a267edc7c0144f30800660dc3b182ca986bf8830
zacharia <65287959+zach123abc@users.noreply.github.com>
2025-10-05 09:46:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import random, string
 
+app = Flask(__name__)
+app.secret_key = ""
+
+
+@app.route("/generate", methods=["GET", "POST"])
+def generate():
+    if request.method == "POST":
+        length = int(request.form["length"])  # get the length from form
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = "".join(random.choice(characters) for _ in range(length))
+        return render_template("generate.html", password=password)
+    return render_template("generate.html", password=None)
