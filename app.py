3e557cc770567297788abe2a6b3fd6649d90cda9
zacharia <65287959+zach123abc@users.noreply.github.com>
2025-10-05 13:24:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def scam_awareness():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
