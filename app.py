34fc20fe3c006967c660420257abc5c8cd9c9d18
zacharia <65287959+zach123abc@users.noreply.github.com>
2025-10-05 09:49:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
+
 from flask import Flask, render_template, request, redirect, url_for, session
 import random, string
 
 app = Flask(__name__)
-app.secret_key = ""
+app.secret_key = "why_u_looking_at_secerts" 
 
+@app.route("/")
+def home():
+    return redirect(url_for("menu"))
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
 
 @app.route("/generate", methods=["GET", "POST"])
 def generate():
     if request.method == "POST":
-        length = int(request.form["length"])  # get the length from form
+        length = int(request.form["length"])
         characters = string.ascii_letters + string.digits + string.punctuation
         password = "".join(random.choice(characters) for _ in range(length))
         return render_template("generate.html", password=password)
     return render_template("generate.html", password=None)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
